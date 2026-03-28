35d1c7b512988994c785c4aeb4e93c498d5cb493
Sviatron <sviashe@gmail.com>
2026-03-28 12:15:08 +0200

    attepmt 2

---
+++ main.js
@@ -0,0 +1,16 @@
+const button = document.querySelector(".btn");
+const text = document.querySelector(".p");
+button.textContent = "not a bouton";
+
+const title = document.querySelector(".title");
+const img = document.querySelector(".img");
+img.src =
+  "https://image.petmd.com/files/inline-images/corgi-dog.jpg?VersionId=.7EViu0bVKO1uj6qiJmnvkKDCL1XU48h";
+
+const link = document.querySelector(".link");
+link.href = "https://www.instagram.com/";
+const img2 = document.querySelector(".img2");
+img2.alt = "DAWG";
+
+const div = document.querySelector(".div");
+div.firstChild.textContent = "not sup guys";
